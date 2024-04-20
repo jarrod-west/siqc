f8c7fafa830f301954306046b0ec9cd4a1eda556
Jarrod West <jnw.dev@gmail.com>
2024-04-20 22:40:12 +1000

    Contact flow export

---
+++ export/export.py
@@ -0,0 +1,69 @@
+import boto3
+from dotenv import dotenv_values
+import json
+from pathlib import Path
+
+from src.logger import logger
+
+FLOW_NAMES = [
+  "Callback_Inbound"
+]
+
+FLOW_EXPORT_DIR = Path(__file__).parent.joinpath("flows")
+FLOW_CONTENT_DIR = FLOW_EXPORT_DIR.joinpath("content")
+
+
+def get_flow_arns(client, instance_arn):
+  # Map contact flow names to their ARNs
+  flows = {}
+
+  logger.info(f"Mapping flow ARNs")
+  paginator = client.get_paginator("list_contact_flows")
+
+  for page in paginator.paginate(InstanceId=instance_arn):
+    for flow in page["ContactFlowSummaryList"]:
+      flows[flow["Name"]] = flow["Arn"]
+
+  return flows
+
+def write_file(directory, name, content):
+  with open(directory.joinpath(name + ".json"), "w") as outfile:
+    json.dump(content, outfile, indent=2)
+
+def export_flow(client, flow_name, flow_arn, instance_arn):
+  logger.info(f"Exporting flow {flow_name}")
+
+  # Describe the flow
+  flow = client.describe_contact_flow(
+    InstanceId=instance_arn,
+    ContactFlowId=flow_arn
+  )["ContactFlow"]
+
+  # Separate out the content and save both to files
+  content = flow.pop("Content")
+  write_file(FLOW_EXPORT_DIR, flow_name, flow)
+  write_file(FLOW_CONTENT_DIR, flow_name, json.loads(content))
+
+  logger.info(f"Flow export complete")
+
+def export():
+  parameters = dotenv_values()
+  instance_arn = parameters["ConnectInstanceArn"]
+
+  # Create the target directories
+  FLOW_CONTENT_DIR.mkdir(exist_ok=True, parents=True)
+
+  client = boto3.client("connect")
+
+  # Map the ids
+  flow_arns = get_flow_arns(client, instance_arn)
+
+  # Export each flow
+  for flow_name in FLOW_NAMES:
+    export_flow(client, flow_name, flow_arns[flow_name], instance_arn)
+
+  logger.info("Export complete")
+
+
+if __name__ == '__main__':
+  export()
